ad6be1fe58d3f831f696aad133c398a37ce2e814
camelgod <god@e-grotto.faith>
2017-09-28 11:24:35 +0200

    Updated configs for herbstluftwm

---
+++ herbstluftwm/tray.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ 
+trayer --edge top --align right --distancefrom top --distance 16 --widthtype pixel --width 77 --heighttype pixel \
+	    --height 16 --expand true --transparent true \
+	    				        --alpha 255
